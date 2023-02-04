ba111488cd6b52e52b5c5d6170ba3926f4a1d777
Cube Number Zero <cubenumberzero@gmail.com>
2023-02-04 11:02:58 -0500

    Cleaned up my code
    I put stuff into functions so we can tell what is going on

--- roots_drawing.gd
+++ roots_drawing.gd
@@ -32,46 +32,56 @@ func _process(delta):
 	if Input.is_mouse_button_pressed(1):
 		if not drawing:
 			# Find the closest node to the mouse cursor
-			var closest_node = tree_base.get_closest_node_to_point(mouse_position)
-			if closest_node[1] <= pow(MAXIMUM_DRAW_SNAP_DISTANCE, 2):
-				# Begin drawing from the closest node
-				drawing = true
-				parent_node = closest_node[0]
-				first_node_in_branch = true
+			snap_to_node(mouse_position)
 	else:
 		drawing = false
 		
 	if drawing:
-		var squared_distance_from_parent_node =\
-			mouse_position.distance_squared_to(parent_node.get_global_position())
-		
-		if squared_distance_from_parent_node >= pow(MINIMUM_NODE_DISTANCE, 2):
-			# Test to see if the node collides with any other nodes
-			if tree_base.test_collision(mouse_position,\
-				MINIMUM_UNRELATED_NODE_DISTANCE, parent_node.gather_nearby_nodes(1)):
-				# The root being drawn collided with another root!
+		if not too_close_to_previous_node(mouse_position):
+			if colliding_with_node(mouse_position):
 				drawing = false
 			else:
-				# Create the next node
-				var new_node = NEW_NODE.instance()
-				parent_node.add_child(new_node)
-				new_node.position = mouse_position - parent_node.get_global_position()
-				new_node.get_child(0).get_child(0).set_point_position(0, \
-					parent_node.get_global_position() - new_node.get_global_position())
-				
-				# if first_node_in_branch:
-					# tree_base.calculate_branch_ID("0")
-					# drawing_branch_ID = new_node.branch_ID
-				# else:
-					# new_node.branch_ID = drawing_branch_ID
-					
-				parent_node = new_node
-				# Play sound
-				SoundPlayer.play_grow_root(mouse_position)
-				# Recalculate the longest distance
-				max_root_length = tree_base.get_longest_distance_from_origin()
-				tree_base.get_size()
+				add_node(mouse_position)
 	
 	if Input.is_mouse_button_pressed(2):
 		# The player is right clicking
 		tree_base.try_to_erase_at_location(mouse_position, ERASER_RADIUS)
+		update_root_network()
+		
+func snap_to_node(loc):
+	var closest_node = tree_base.get_closest_node_to_point(loc)
+	if closest_node[1] <= pow(MAXIMUM_DRAW_SNAP_DISTANCE, 2):
+		# Begin drawing from the closest node
+		drawing = true
+		parent_node = closest_node[0]
+		first_node_in_branch = true
+
+func update_root_network():
+	max_root_length = tree_base.get_longest_distance_from_origin()
+	tree_base.get_size()
+
+func add_node(loc):
+	var new_node = NEW_NODE.instance()
+	parent_node.add_child(new_node)
+	new_node.position = loc - parent_node.get_global_position()
+	new_node.get_child(0).get_child(0).set_point_position(0, \
+		parent_node.get_global_position() - new_node.get_global_position())
+		
+	# if first_node_in_branch:
+		# tree_base.calculate_branch_ID("0")
+		# drawing_branch_ID = new_node.branch_ID
+	# else:
+		# new_node.branch_ID = drawing_branch_ID
+	
+	parent_node = new_node
+	# Play sound
+	SoundPlayer.play_grow_root(loc)
+	
+	update_root_network()
+
+func colliding_with_node(loc):
+	return tree_base.test_collision(loc, MINIMUM_UNRELATED_NODE_DISTANCE, parent_node.gather_nearby_nodes(1))
+
+func too_close_to_previous_node(loc):
+	var squared_distance_from_parent_node = loc.distance_squared_to(parent_node.get_global_position())
+	return squared_distance_from_parent_node < pow(MINIMUM_NODE_DISTANCE, 2)
