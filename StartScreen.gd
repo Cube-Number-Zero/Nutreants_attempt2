eede19292d0df00b8abe96f0f400b85a57520952
Johnethon <98053051+Johnethon@users.noreply.github.com>
2023-02-04 23:16:31 -0500

    Main Menu 'finished'

--- StartScreen.gd
+++ StartScreen.gd
@@ -14,4 +14,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var mouse_position = get_viewport().get_mouse_position()
+	if mouse_position.x >= $NewGame.rect_position.x and mouse_position.x <= $NewGame.rect_position.x + 320:
+		if mouse_position.y >= $NewGame.rect_position.y and mouse_position.y <= $NewGame.rect_position.x + 160:
+			get_tree().change_scene("World.tscn")
+	if mouse_position.x >= $NewGame2.rect_position.x and mouse_position.x <= $NewGame2.rect_position.x + 320:
+		if mouse_position.y >= $NewGame2.rect_position.y and mouse_position.y <= $NewGame2.rect_position.x + 160:
+			get_tree().quit()
